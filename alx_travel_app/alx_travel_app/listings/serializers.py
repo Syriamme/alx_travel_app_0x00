e5027b51a272925a3780defb05ad3e1d31e403e7
Syriamme <roridaniel01@gmail.com>
2024-12-26 00:01:37 +0300

    serialization

---
+++ alx_travel_app/alx_travel_app/listings/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Listing, Booking
+
+class ListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = ['id', 'title', 'description', 'price_per_night', 'location', 'created_at']
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = ['id', 'listing', 'user_name', 'check_in_date', 'check_out_date', 'created_at']
